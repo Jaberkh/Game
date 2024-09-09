3c4289d9ad1253e5261d9d0b366c0ae3188cd49a
Jaber <khodadadi.jaber@live.com>
2024-09-09 23:29:40 +0330

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -2,7 +2,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppI
 from telegram.ext import ApplicationBuilder, CommandHandler
 
 # توکن بات خود را وارد کنید
-TOKEN = 'YOUR_BOT_TOKEN'
+TOKEN = '7200440128:AAFE1aOYaMj0Eqozc0jp6DDDDlt-Xad9bic'
 
 # تابع شروع
 async def start(update: Update, context):
